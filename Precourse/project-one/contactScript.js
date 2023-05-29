ce778deaa98fd276659d5d2ec1f123d343634ff2
Janet Suryadi <janet@suryadi.id.au>
2023-05-29 10:54:03 +1000

    First time commiting from git bash

--- Precourse/project-one/contactScript.js
+++ Precourse/project-one/contactScript.js
@@ -32,6 +32,46 @@ const sendMessage = function (event) {
         JSON.stringify(currentMessages)
     );
 
+    // Calling the renderMessages function so the message feed is automatically updated.
+    renderMessages();
+
+}
+
+const deleteMessages = function (event) {
+    if (window.sessionStorage.getItem("messages")) {
+        window.sessionStorage.removeItem("messages");
+    }
+
+    renderMessages();
+}
+
+const renderMessages = function () {
+    let currentMessages = [];
+
+    if (window.sessionStorage.getItem("messages")) {
+
+        currentMessages = JSON.parse(
+            window.sessionStorage.getItem("messages")
+        );
+    }
+
+    let listItems = [];
+    for (let i = 0; i < currentMessages.length; i++) {
+        let listItem = "";
+        const currentMessage = currentMessages[i];
+
+        // By using backticks, we can append a template string to listItem, which allows us to use HTML tags and templating in the string.
+
+        // Dt is the Description Title tag, Dd is the Description Body tag.
+
+        listItem += `<dt>${currentMessage.fullName} - ${currentMessage.email}</dt>`
+        listItem += `<dd>${currentMessage.message}</dd> <br/>`
+        listItems.push(listItem);
+    }
+
+    // Actually displaying the messages.
+    let descList = document.getElementById("currentMessages");
+    descList.innerHTML = listItems.join("");
 }
 
 
@@ -43,6 +83,7 @@ const sendMessage = function (event) {
 /*=============================================
 =            Script Assignments            =
 =============================================*/
+renderMessages();
 
 
 
